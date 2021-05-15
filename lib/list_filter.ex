273f2c7ed463251dc03bc8ed38915fa13feb227f
asrlopes <a.junior.srlopes@gmail.com>
2021-05-14 21:08:46 -0400

    solução provisória

---
+++ lib/list_filter.ex
@@ -0,0 +1,32 @@
+defmodule ListFilter do
+  def call(list), do: filter(list)
+
+  defp filter(list) do
+    list
+    |> get_numeric_items
+    |> get_odd_numbers
+  end
+
+  # defp convert_string_to_number(list)
+
+  # defp get_numeric_items(list), do: Enum.filter(list, fn item -> Kernel.is_number(item) end)
+
+  defp get_numeric_items(list), do: Enum.map(list, fn item -> validate_number(item) end)
+
+  defp validate_number(item) do
+    regex = Regex.compile!("^[0-9]*$")
+
+    with true <- Regex.match?(regex, item) do
+      String.to_integer(item)
+    else
+      _ -> 0
+    end
+  end
+
+  defp get_odd_numbers(numeric_items) do
+    Enum.filter(numeric_items, fn item -> odd_number(item) end)
+  end
+
+  defp odd_number(item) when rem(item, 2) == 0, do: nil
+  defp odd_number(item) when rem(item, 2) != 0, do: item
+end
